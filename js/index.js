d043a2a2bb54e492fd196a03e6b0056d6e8d2257
zamwel <otengs16@gmail.com>
2021-12-08 23:39:37 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -205,9 +205,7 @@ function knuthShuffle (arr) {
 setSearch()
 
 $(document).ready(function () {
-  if(localStorage.getItem("pay") !== null){
-   windows.open("/payment.html") 
-  }
+  
   $('#cc-table tbody').on('click', 'td', function () {
     var column_num = parseInt($(this).index())
     //console.log('column index:' + column_num);
