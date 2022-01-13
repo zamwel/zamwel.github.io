08aa3ffe63d0014552df0178b530289ea8537050
zamwel <otengs16@gmail.com>
2022-01-13 16:08:06 +0000

    Update account.js

--- js/account.js
+++ js/account.js
@@ -90,9 +90,9 @@ function loadhistorymaker () {
         li = `<li>${e.doc.data().data}</li>`
         ht += li
       })
-        if(ht.includes("undefined")){
+        /*if(ht.includes("undefined")){
             document.querySelector(".active-history").innerHTML = ht.replace("undefined","");
-        }
+        }*/
     document.querySelector(".active-history").innerHTML = ht;
     })
   
